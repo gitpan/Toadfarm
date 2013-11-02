fd70ebcc6aa19a6f42c4f57c7e4aeae64a6d851a
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-11-02 16:17:42 +0000

    Import of JHTHORSEN/Toadfarm-0.17 from CPAN.
    gitpan-cpan-distribution: Toadfarm
    gitpan-cpan-version:      0.17
    gitpan-cpan-path:         JHTHORSEN/Toadfarm-0.17.tar.gz
    gitpan-cpan-author:       JHTHORSEN
    gitpan-cpan-maturity:     released

--- lib/Ubic/Service/Toadfarm.pm
+++ lib/Ubic/Service/Toadfarm.pm
@@ -121,17 +121,18 @@ response is not important, the important thing is that the server responds.
 sub status_impl {
   my $self = shift;
   my $listen = $self->{hypnotoad}{listen}[0];
+  my $resource = $self->{hypnotoad}{status_resource} || "/ubic-status";
   my %args = ( connect_timeout => 5, request_timeout => 20 );
   my $tx;
 
   require Mojo::UserAgent;
 
   $listen =~ s!\*!localhost!;
-  $tx = Mojo::UserAgent->new(%args)->head("$listen/ubic-status");
+  $tx = Mojo::UserAgent->new(%args)->head($listen .$resource);
   warn $tx->res->code // 'No HTTP code', "\n" if DEBUG;
 
-  if($tx->res->code) {
-    return result 'running';
+  if(my $code = $tx->res->code) {
+    return result "running", "status $code";
   }
   else {
     return result 'not running';
